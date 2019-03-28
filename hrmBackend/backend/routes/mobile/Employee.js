34ee698e81922941c14150206770f1117c5e8b10
Mohsin <mohsin@mmcgbl.com>
2019-03-28 14:50:54 +0500

    added seprate routes for web and mobile

--- hrmBackend/backend/routes/mobile/Employee.js
+++ hrmBackend/backend/routes/mobile/Employee.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const user = require('../Models/Admin');
+const users = require('../../Models/Admin');
+const roles = require('../../Models/roles');
+const userRole = require('../../Models/userRole');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const joi = require('joi');
 const jwt = require('jsonwebtoken');
-const userRole = require('../Models/userRole')
-const Roles = require('../Models/roles')
+const auth = require('../../auth/authentication')
 
 router.post('/login', async (req, res) => {
 
@@ -14,24 +15,23 @@ router.post('/login', async (req, res) => {
         password: joi.required()
 
     }
+    
     const { error } = joi.validate(req.body, schema);
     if (error) return res.send({ 'message': 'Failed', 'err': error.details[0].message,'code': 500 });
 
     try {
-        let User = await user.findAll({ 
+        let User = await users.findAll({ 
             attributes:['Firstname','Lastname',['Id','user_id'],'Email','Password'],
             include:[{
                 attributes:[['Role_name','Role'],['Id','Role_id']],
-                model: Roles,
+                model: roles,
                 where: {
-                    Role_name: 'admin'
+                    Role_name: 'employee'
                 }
             }],
             raw: true, 
             // limit: 1, 
-            where: { 
-                Email: req.body.email 
-            } 
+            where: { Email: req.body.email } 
         });
 
         if (User.length <= 0) return res.send({ 'message': 'failed', err: 'Invalid email or password','code': 500 });
