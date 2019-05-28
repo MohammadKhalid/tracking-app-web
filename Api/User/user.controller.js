2c989043962269b909469d10780ddcf42a2ba673
Mohsin <mohsin@mmcgbl.com>
2019-05-28 12:55:09 +0500

    added user module apis

--- Api/User/user.controller.js
+++ Api/User/user.controller.js
@@ -5,8 +5,51 @@ const jwt = require('jsonwebtoken')
 
 
 module.exports = {
-    login: (req, res) => {
+    login: async (req, res) => {
+        let { email, password } = req.body
+        const error = validationResult(req)
+        if (!error.isEmpty()) {
+            return res.json({ code: 500, data: '', message: error.array() });
+        }
 
+        try {
+            let user = await userModel.findOne({
+                where: {
+                    email: email
+                }
+            })
+
+            if (!user) {
+                return res.json({
+                    code: 500,
+                    data: '',
+                    message: 'Invalid email or password.'
+                });
+            }
+
+            const isPassword = await bcrypt.compare(password, user.password);
+            if (!isPassword) {
+                return res.json({
+                    code: 500,
+                    data: '',
+                    message: 'Invalid email or password.'
+                });
+            }
+
+            const token = jwt.sign({ user }, 'jwtPrivateKey');
+            return res.json({
+                code: 200,
+                data: '',
+                token: token,
+                message: 'Login succesful.'
+            });
+        } catch (error) {
+            return res.json({
+                code: 500,
+                data: '',
+                message: 'API failed.'
+            });
+        }
     },
 
     create: async (req, res) => {
@@ -17,11 +60,38 @@ module.exports = {
         }
 
         try {
-            let checkUser = await userModel.findone({ where: { Email: req.body.email } });
+            let checkUser = await userModel.findOne({ where: { email: email } });
+
+            if (checkUser) {
+                return res.json({
+                    code: 500,
+                    data: '',
+                    message: 'User already exists.'
+                });
+            }
+            const passwordHash = await bcrypt.hash(password, 10);
+
+            const user = await userModel.create({
+                adminId: adminId,
+                email: email,
+                password: passwordHash,
+                phone: phone,
+                firstName: firstname,
+                lastName: lastname,
+                image: image,
+                isActive: 1
+            })
+            if (!user) {
+                return res.json({
+                    code: 500,
+                    data: '',
+                    message: 'Unable to add employee.'
+                });
+            }
             return res.json({
-                code: 500,
-                data: checkUser,
-                message: 'API failed.'
+                code: 200,
+                data: '',
+                message: 'Employee created.'
             });
         } catch (error) {
             return res.json({
@@ -29,6 +99,61 @@ module.exports = {
                 data: '',
                 message: 'API failed.'
             });
+        }
+    },
+
+    viewAllEmployee: async (req, res) => {
+        try {
+            let { adminId } = req.params
+
+            let users = await userModel.findAll({
+                where: {
+                    adminId: adminId
+                }
+            })
+            return res.json({
+                code: 200,
+                data: users,
+                message: ''
+            });
+        } catch (error) {
+            return res.json({
+                code: 500,
+                data: '',
+                message: 'API failed.'
+            });
+        }
+    },
+
+    editEmployee: async (req, res) => {
+        try {
+            const { adminId, firstname, lastname, email,phone } = req.body;
+            const {employeeId} = req.params;
+            let updated = await users.update({
+                firstName: firstname,
+                lastName: lastname,
+                email: email,
+                phone: phone,
+            }, {
+                    where: {
+                        Id: employeeId,
+                    }
+                }
+            )
+
+            res.send({
+                'message': 'updated successfully',
+                'data': updated,
+                "code": '200'
+            })
+
+        } catch (error) {
+            res.send({
+                'message': 'API failed',
+                'data': error,
+                "code": '500'
+            })
+
         }
     }
 }
