4a386235a7595aa00f5ba2776b532f77908102c4
Mohsin <mohsin@mmcgbl.com>
2019-06-12 17:51:59 +0500

    updated personal notes api

--- api/personalNotes/personalNotes.routes.js
+++ api/personalNotes/personalNotes.routes.js
@@ -6,10 +6,6 @@ const { check } = require('express-validator/check');
 router.post('/saveNotes',personalNotesController.saveNotes)
 
 router.get('/getNotes/:userId',personalNotesController.getNotes)
-router.get('/getNoteById/:taskId',personalNotesController.getNoteById)
 
-router.delete('/deleteNote/:taskId',personalNotesController.deleteNote)
-
-router.put('/editNotes/:taskId',personalNotesController.editNotes)
 
 module.exports = router;
